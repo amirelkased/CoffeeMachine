01a3a3d93689e91ae1ffc840c2ece2cd77153fd9
amirelkased <amirelkased8558@gmail.com>
2022-09-03 17:45:24 +0200

    Indentation update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,8 +11,9 @@ public class Main {
 
     public static void main(String[] args) {
         while (true) {
-            System.out.println("Write action (buy, fill, take, remaining, exit): ");
+            System.out.print("Write action (buy, fill, take, remaining, exit): ");
             String ope = scanner.nextLine();
+            System.out.println();
             switch (ope) {
                 case "buy" -> buy();
                 case "fill" -> fill();
@@ -23,23 +24,24 @@ public class Main {
                 }
                 default -> System.out.println("Invalid input");
             }
+            System.out.println();
         }
     }
 
     private static void fill() {
-        System.out.println("Write how many ml of water you want to add:");
+        System.out.print("Write how many ml of water you want to add:");
         int water = scanner.nextInt();
-        System.out.println("Write how many ml of milk you want to add: ");
+        System.out.print("Write how many ml of milk you want to add: ");
         int milk = scanner.nextInt();
-        System.out.println("Write how many grams of coffee beans you want to add: ");
+        System.out.print("Write how many grams of coffee beans you want to add: ");
         int coffeeBeans = scanner.nextInt();
-        System.out.println("Write how many disposable cups of coffee you want to add: ");
+        System.out.print("Write how many disposable cups of coffee you want to add: ");
         int disposableCup = scanner.nextInt();
         Main.fillMachine(water, milk, coffeeBeans, disposableCup);
     }
 
     private static void buy() {
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
+        System.out.print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
         String check;
         if (scanner.hasNextLine()) {
             check = scanner.nextLine();
